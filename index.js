b7998432228942b8abe23b560beab307aead2e9d
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2016-11-08 19:39:06 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var request = require('request')
+  , FeedParser = require('feedparser')
+
+var feedUrl = process.env.FEED_URL
+
+function fetch(feed) {
+  // Define our streams
+  var req = request(feed, {timeout: 10000, pool: false});
+  req.setMaxListeners(50);
+  // Some feeds do not respond without user-agent and accept headers.
+  req.setHeader('user-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36');
+  req.setHeader('accept', 'text/html,application/xhtml+xml');
+
+  var feedparser = new FeedParser();
+  var done = (err) => {
+    console.log(err);
+  }
+
+  // Define our handlers
+  req.on('error', done);
+  req.on('response', function(res) {
+    if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
+    // And boom goes the dynamite
+    res.pipe(feedparser);
+  });
+
+  feedparser.on('error', done);
+  feedparser.on('end', done);
+  feedparser.on('readable', function() {
+    var post;
+    while (post = this.read()) {
+      console.log(JSON.stringify(post, ' ', 4));
+    }
+  });
+}
+
+setInterval(fetch.bind(this, feedUrl), 5000);
