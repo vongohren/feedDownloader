539294c37b830e7698afc21aca216aa65fe1ff3d
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2016-11-10 23:04:39 +0100

    completeness naa

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const request = require('request')
 const FeedParser = require('feedparser')
+const fs = require('fs')
 
 const feedUrl = process.env.FEED_URL
 const desicionMaker = require('./lib/desicion-maker.js');
+const location = './';
 
 function fetch(feed) {
   // Define our streams
@@ -31,7 +33,12 @@ function fetch(feed) {
   feedparser.on('end', async function() {
     for(let post of posts) {
       let download = await desicionMaker.decide(post)
-      if(download) console.log(post.title)
+      if(download) {
+        const requestet = request(post.link)
+        requestet.setHeader('user-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36')
+        requestet.setHeader('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8');
+        requestet.pipe(fs.createWriteStream(`${post.title}.torrent`))
+      }
     }
   });
   feedparser.on('readable', function() {
